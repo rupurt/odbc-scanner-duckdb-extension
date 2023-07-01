f733053c9db745172a82de8adfbad008c2a98254
Alex Kwiatkowski <alex+git@fremantle.io>
2023-07-01 10:22:38 -0700

    use nix flake scripts to run build & test apps

--- flake.nix
+++ flake.nix
@@ -4,16 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    zig-overlay = {
-      url = "github:mitchellh/zig-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
-    self,
     flake-utils,
-    zig-overlay,
     nixpkgs,
     ...
   }: let
@@ -21,48 +15,75 @@
     outputs = flake-utils.lib.eachSystem systems (system: let
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [
-          zig-overlay.overlays.default
-        ];
       };
     in rec {
       # packages exported by the flake
-      packages = {
-        zig-build-fast = pkgs.writeScriptBin "zig_build_fast" ''
-          ${pkgs.zigpkgs.master}/bin/zig build \
-            -Dopenssl-include-dir=${pkgs.openssl.dev}/include \
-            -Doptimize=ReleaseFast \
-            -Dbuild-shell
-        '';
-        zig-build-small = pkgs.writeScriptBin "zig_build_small" ''
-          ${pkgs.zigpkgs.master}/bin/zig build \
-            -Dopenssl-include-dir=${pkgs.openssl.dev}/include \
-            -Doptimize=ReleaseSmall \
-            -Dbuild-shell
-        '';
-        zig-build-debug = pkgs.writeScriptBin "zig_build_debug" ''
-          ${pkgs.zigpkgs.master}/bin/zig build \
-            -Dopenssl-include-dir=${pkgs.openssl.dev}/include \
-            -Doptimize=Debug \
-            -Dbuild-shell
-        '';
-      };
+      packages = {};
 
       # nix run
       apps = {
-        build-fast = {
+        generate-dot-clangd = {
           type = "app";
-          program = "${self.packages.${system}.zig-build-fast}/bin/zig_build_fast";
+          program = toString (pkgs.writeScript "generate-dot-clangd" ''
+            echo "CompileFlags:" > .clangd
+            echo "  Add:" >> .clangd
+            echo "    - -I$PWD/src/include" >> .clangd
+            echo "    - -I${pkgs.unixODBC}/include" >> .clangd
+            echo "    - -I$PWD/duckdb/src/include" >> .clangd
+            echo "    - -I$PWD/duckdb/third_party/re2" >> .clangd
+            echo "    - -I$PWD/duckdb/third_party/parquet" >> .clangd
+            echo "    - -I$PWD/duckdb/third_party/thrift" >> .clangd
+            echo "    - -I$PWD/duckdb/third_party/utf8proc" >> .clangd
+            echo "    - -I$PWD/duckdb/third_party/tools/sqlite3_api_wrapper/include" >> .clangd
+            echo "    - -I$PWD/duckdb/third_party/tools/sqlite3_api_wrapper/sqlite3_udf_api/include" >> .clangd
+            echo "    - -I$PWD/duckdb/third_party/imdb/include" >> .clangd
+          '');
         };
-        build-small = {
+        test = {
           type = "app";
-          program = "${self.packages.${system}.zig-build-small}/bin/zig_build_small";
+          program = toString (pkgs.writeScript "test" ''
+            export PATH="${pkgs.lib.makeBinPath (
+              with pkgs; [
+                git
+                gnumake
+                cmake
+                ninja
+                llvmPackages_16.clang
+                openssl
+                unixODBCDrivers.psql
+              ]
+            )}:$PATH"
+            export CC=${pkgs.llvmPackages_16.clang}/bin/clang
+            export CXX=${pkgs.llvmPackages_16.clang}/bin/clang++
+
+            make \
+              GEN=ninja \
+              test CLIENT_FLAGS="-DODBC_CONFIG=${pkgs.unixODBC}/bin/odbc_config"
+          '');
         };
-        build-debug = {
+        build = {
           type = "app";
-          program = "${self.packages.${system}.zig-build-debug}/bin/zig_build_debug";
+          program = toString (pkgs.writeScript "build" ''
+            export PATH="${pkgs.lib.makeBinPath (
+              with pkgs; [
+                git
+                gnumake
+                cmake
+                ninja
+                llvmPackages_16.clang
+                openssl
+                unixODBCDrivers.psql
+              ]
+            )}:$PATH"
+            export CC=${pkgs.llvmPackages_16.clang}/bin/clang
+            export CXX=${pkgs.llvmPackages_16.clang}/bin/clang++
+
+            make \
+              GEN=ninja \
+              CLIENT_FLAGS="-DODBC_CONFIG=${pkgs.unixODBC}/bin/odbc_config"
+          '');
         };
-        default = apps.build-fast;
+        default = apps.build;
       };
 
       # nix fmt
@@ -71,8 +92,15 @@
       # nix develop -c $SHELL
       devShells.default = pkgs.mkShell {
         packages = [
+          pkgs.git
+          pkgs.gnumake
+          pkgs.cmake
+          pkgs.ninja
+          pkgs.llvmPackages_16.clang
           pkgs.openssl
-          pkgs.zigpkgs.master
+          pkgs.unixODBC
+          pkgs.unixODBCDrivers.psql
+          pkgs.postgresql_15
         ];
       };
     });
