61f3844b354bed32b6ba5810fd4099f88e75f09f
Alex Kwiatkowski <alex+git@fremantle.io>
2023-07-17 00:16:28 -0700

    add odbc scan to_string returning table_name

--- src/odbc_scan.cpp
+++ src/odbc_scan.cpp
@@ -5,13 +5,20 @@
 #include "duckdb/function/table_function.hpp"
 
 namespace duckdb {
+static string OdbcScanToString(const FunctionData *bind_data_p) {
+  D_ASSERT(bind_data_p);
+
+  auto bind_data = (const OdbcScanBindData *)bind_data_p;
+  return bind_data->table_name;
+}
+
 OdbcScanFunction::OdbcScanFunction()
     : TableFunction(
           "odbc_scan",
           {LogicalType::VARCHAR, LogicalType::VARCHAR, LogicalType::VARCHAR},
           OdbcScan, OdbcScanBind, OdbcScanInitGlobalState,
           OdbcScanInitLocalState) {
-  // to_string = PostgresScanToString;
+  to_string = OdbcScanToString;
   // projection_pushdown = true;
 }
 } // namespace duckdb
