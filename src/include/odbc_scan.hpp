97bb71d7df1a7c53b1d380e97f3278efbac9110d
Alex Kwiatkowski <alex+git@fremantle.io>
2023-07-23 23:24:38 -0700

    wip: add odbc_attach function
    - [x] return column descriptions
    - [x] bind columns from column descriptions
    - [x] fetch results from SQLTables
    - [x] hydrate views from Db2
    - [ ] hydrate views postgres Db2
    - [ ] handle SQL_ARD_TYPE
    - [ ] handle other unknown types (-98, -307)??? what are they. Not in sql.h
    - [ ] handle parameters for catalog, schema & table name filtering
    - [ ] handle parameter for table type
    - [ ] add optional schema prefix
    - [ ] implement predicate pushdown on attached views

--- src/include/odbc_scan.hpp
+++ src/include/odbc_scan.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "odbc.hpp"
+#include "odbc_mapping.hpp"
 
 #include "duckdb.hpp"
 #include "duckdb/common/exception_format_value.hpp"
@@ -32,33 +33,6 @@ public:
   bool Equals(const FunctionData &other) const override { throw NotImplementedException(""); }
 };
 
-struct OdbcColumnBinding {
-  OdbcColumnBinding(OdbcColumnDescription col_desc, SQLINTEGER row_array_size) {
-    column_buffer_length = col_desc.length;
-    sql_data_type = col_desc.sql_data_type;
-    c_data_type = col_desc.c_data_type;
-
-    strlen_or_ind = new SQLLEN[row_array_size];
-    memset(strlen_or_ind, 0, row_array_size);
-
-    buffer = new unsigned char[row_array_size * column_buffer_length];
-    memset(buffer, 0, row_array_size * column_buffer_length);
-  }
-  ~OdbcColumnBinding() {
-    // TODO:
-    // - why does freeing these cause a segfault?
-    // - should I copy the values when setting the DuckDB output?
-    // delete[] strlen_or_ind;
-    // delete[] buffer;
-  }
-
-  SQLULEN column_buffer_length;
-  SQLSMALLINT sql_data_type;
-  SQLSMALLINT c_data_type;
-  SQLLEN *strlen_or_ind;
-  unsigned char *buffer;
-};
-
 struct OdbcScanLocalState : public LocalTableFunctionState {
   OdbcScanLocalState(SQLINTEGER _row_array_size)
       : offset(0), row_status(vector<SQLUSMALLINT>(_row_array_size)) {}
@@ -75,5 +49,10 @@ struct OdbcScanGlobalState : public GlobalTableFunctionState {
 class OdbcScanFunction : public TableFunction {
 public:
   OdbcScanFunction();
+};
+
+class OdbcScanFunctionFilterPushdown : public TableFunction {
+public:
+  OdbcScanFunctionFilterPushdown();
 };
 } // namespace duckdb
